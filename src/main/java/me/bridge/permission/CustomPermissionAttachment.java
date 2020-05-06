1b630af17bb4bd46e131787030ef8f429dde70d7
7x6 <ilhantemel@outlook.com>
2020-05-06 21:44:09 +0200

    Initial commit

---
+++ src/main/java/me/bridge/permission/CustomPermissionAttachment.java
@@ -0,0 +1,63 @@
+package me.bridge.permission;
+
+import me.bridge.permission.reflection.BasicReflection;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permissible;
+import org.bukkit.permissions.PermissionAttachment;
+import org.bukkit.plugin.Plugin;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+public class CustomPermissionAttachment extends PermissionAttachment {
+
+    private static final Field PERMISSIONS_FIELD =
+            BasicReflection.fetchField(PermissionAttachment.class, "permissions");
+    private static final Field PERMISSIBLE_FIELD =
+            BasicReflection.fetchField(PermissionAttachment.class, "permissible");
+
+    static {
+        CustomPermissionAttachment.PERMISSIONS_FIELD.setAccessible(true);
+        CustomPermissionAttachment.PERMISSIONS_FIELD.setAccessible(true);
+    }
+
+
+    public CustomPermissionAttachment(Plugin plugin, Permissible Permissible) {
+        super(plugin, Permissible);
+    }
+
+    public CustomPermissionAttachment(Plugin plugin, Player player) {
+        super(plugin, PermissibleUtil.getPermissible(player));
+    }
+
+    public void recalculatePermissions() {
+        ((Permissible) BasicReflection.invokeField(CustomPermissionAttachment.PERMISSIBLE_FIELD, this))
+                .recalculatePermissions();
+    }
+
+    @Override
+    public void setPermission(String name, boolean value) {
+        this.addPermission(name, value);
+    }
+
+    @Override
+    public void unsetPermission(String name) {
+        this.removePermission(name);
+    }
+
+    @SuppressWarnings("unchecked")
+    public void addPermission(String permission, boolean value) {
+        ((Map<String, Boolean>) BasicReflection.invokeField(CustomPermissionAttachment.PERMISSIONS_FIELD, this))
+                .put(permission.toLowerCase(), value);
+    }
+
+    @SuppressWarnings("unchecked")
+    public void removePermission(String permission) {
+        ((Map<String, Boolean>) BasicReflection.invokeField(CustomPermissionAttachment.PERMISSIONS_FIELD, this))
+                .remove(permission.toLowerCase());
+    }
+
+    public void applyAttachment(Player player) {
+        PermissibleUtil.applyPermissionAttachment(player, this);
+    }
+}
---
+++ src/main/java/me/bridge/permission/reflection/BasicReflection.java
@@ -0,0 +1,132 @@
+package me.bridge.permission.reflection;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
+
+public class BasicReflection {
+
+    /**
+     * Used to invoke a field
+     * @param field The field to invoke
+     * @param object The object where the field is applicable
+     * @exception IllegalArgumentException in case we cannot access the field (Should not happen)
+     * @return invoked field
+     */
+    public static Object invokeField(Field field, Object object) {
+        if (!field.isAccessible())
+            field.setAccessible(true);
+
+        try {
+            return field.get(object);
+        } catch (IllegalAccessException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /**
+     * Used to fetch a field
+     * @param clazz Class where field is applicable
+     * @param fieldName Name of the field we're trying to fetch
+     * @exception IllegalArgumentException in case the field is not found
+     * @return Optional field
+     */
+    public static Field fetchField(Class<?> clazz, String fieldName) {
+        try {
+            return clazz.getDeclaredField(fieldName);
+        } catch (NoSuchFieldException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /**
+     * Used to fetch a constructor
+     * @param clazz Class where the constructor is applicable
+     * @param parameters Constructor in the constructor we're trying to fetch
+     * @exception IllegalArgumentException in case the constructor is not found
+     * @return the fetched constructor
+     */
+    public static Constructor<?> fetchConstructor(Class<?> clazz, Class<?>... parameters) {
+        try {
+            return clazz.getConstructor(parameters);
+        } catch (NoSuchMethodException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /**
+     * Used to invoke a constructor
+     * @param constructor The constructor to invoke
+     * @param parameters The parameters we need to use to invoke the constructor
+     * @exception IllegalArgumentException in case the constructor is not found
+     * @return invoked constructor
+     */
+    public static Object invokeConstructor(Constructor<?> constructor, Object... parameters) {
+        try {
+            return constructor.newInstance(parameters);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /**
+     * Used to fetch a method
+     * @param clazz Class where the method is applicable
+     * @param methodName The name of the method we're fetching
+     * @param parameters The parameters of the method
+     * @return the fetched method
+     */
+    public static Method fetchMethod(Class<?> clazz, String methodName, Class<?>... parameters) {
+        try {
+            return clazz.getDeclaredMethod(methodName, parameters);
+        } catch (NoSuchMethodException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+
+    /**
+     * Used to invoke a method
+     * @param method The method to invoke
+     * @param object The object which contains the method we need to invoke
+     * @param parameters The parameters needed to invoke the method
+     * @return The method's returning object
+     */
+    public static Object invokeMethod(Method method, Object object, Object... parameters) {
+        try {
+            return method.invoke(object, parameters);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /**
+     * Used to find a class by its name
+     * @param name Class' name
+     * @exception IllegalArgumentException in case the class is not found
+     * @return the found class
+     */
+    public static Class<?> getClass(String name) {
+        try {
+            return Class.forName(name);
+        } catch (ClassNotFoundException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /**
+     * Finds a class and calls it back using a {@link BiConsumer}
+     * @param name Class' name
+     * @param callback callback after class is found
+     */
+    public static void getClassCallback(String name, BiConsumer<Class<?>, Throwable> callback) {
+        CompletableFuture<Class<?>> completableFuture = new CompletableFuture<>();
+
+        completableFuture.complete(BasicReflection.getClass(name));
+        completableFuture.whenCompleteAsync(callback);
+    }
+}
+
